e32e27ec8d7a1f99e74b6d3f6a5a3559f0ec2a0d
Shivansh <shivansh01308@gmail.com>
2022-06-07 11:08:36 +0530

    added a build folder

---
+++ build/precache-manifest.41910361c37f666bbba67b0266058a10.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451e13011e568320e57359421ca752cd",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "2f88a62420b9ec6707f6",
+    "url": "/my-website/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "0bfc5dd213ba47444704",
+    "url": "/my-website/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "51d4e3688356e3da01cd",
+    "url": "/my-website/static/css/main.883533a3.chunk.css"
+  },
+  {
+    "revision": "8216fd190bd59193157f",
+    "url": "/my-website/static/js/2.193e9e65.chunk.js"
+  },
+  {
+    "revision": "3d4186a67aba203694a5f10d101a5461",
+    "url": "/my-website/static/js/2.193e9e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f88a62420b9ec6707f6",
+    "url": "/my-website/static/js/3.f7cffe75.chunk.js"
+  },
+  {
+    "revision": "0bfc5dd213ba47444704",
+    "url": "/my-website/static/js/4.7e28fc2c.chunk.js"
+  },
+  {
+    "revision": "51d4e3688356e3da01cd",
+    "url": "/my-website/static/js/main.8af3ef3c.chunk.js"
+  },
+  {
+    "revision": "742010e552447275e6eb",
+    "url": "/my-website/static/js/runtime-main.c3a62704.js"
+  },
+  {
+    "revision": "47542b3abba10454a4685670eb3fcafb",
+    "url": "/my-website/static/media/1200px-BITS_Pilani-Logo.svg.47542b3a.png"
+  },
+  {
+    "revision": "638be90904f9f9248d3c69cb2992c562",
+    "url": "/my-website/static/media/1280px-React-icon.svg.638be909.png"
+  },
+  {
+    "revision": "8b8c665e4b387065c3f2629d8602f733",
+    "url": "/my-website/static/media/1561304768560.8b8c665e.png"
+  },
+  {
+    "revision": "062885b79f7d4459a501b1721cc3aa8d",
+    "url": "/my-website/static/media/1613791081930.062885b7.jpeg"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/my-website/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/my-website/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "6252aadefcdc32c3d977eeb72360a147",
+    "url": "/my-website/static/media/Postman - Postman Student Expert - 2022-02-27.6252aade.6252aade.png"
+  },
+  {
+    "revision": "48126460d35d035b31df572db7dc5cae",
+    "url": "/my-website/static/media/Untitled design.48126460.png"
+  },
+  {
+    "revision": "cc3f11528aab1849458f6effa1ae4040",
+    "url": "/my-website/static/media/Untitled design2.cc3f1152.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/my-website/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "3952f41bd410b0f9b33c3242dc17ccda",
+    "url": "/my-website/static/media/coursera.3952f41b.3952f41b.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/my-website/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "540a8d54d4761b02c4128138ad1dca3d",
+    "url": "/my-website/static/media/glitch.540a8d54.540a8d54.png"
+  },
+  {
+    "revision": "7aaaae456c3b92af5ed08aad3ff1d6d6",
+    "url": "/my-website/static/media/logo-1.7aaaae45.7aaaae45.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/my-website/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "0dc130cbdd92f6703996cc10e85b8883",
+    "url": "/my-website/static/media/postmanlogo.0dc130cb.0dc130cb.png"
+  },
+  {
+    "revision": "2caebe72844491844f83bdcb4a781313",
+    "url": "/my-website/static/media/school-logo-and-font.2caebe72.2caebe72.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/my-website/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/my-website/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
